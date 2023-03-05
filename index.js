dd7c81a9273a1c17623205e8a3e9907c198b5b40
Rafael Gieschke <rafael@gieschke.de>
2023-03-06 00:54:41 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
 #!/usr/bin/env node
 const { chromium } = require('playwright');
-const { argv } = require("process");
-const domain = argv.slice(2);
+const { argv, env } = require("process");
+
+const [ urlOrDomain ] = argv.slice(2);
+const { HEADLESS } = env;
+
+const parseUrlOrDomain = urlOrDomain => {
+  try {
+    return new URL(urlOrDomain);
+  } catch {
+    return new URL(`https://${urlOrDomain}`);
+  }
+};
+
+const origin = parseUrlOrDomain(urlOrDomain).origin;
 
 (async () => {
   const browser = await chromium.launch({
-    headless: false
+    headless: Boolean(HEADLESS),
   });
   const context = await browser.newContext();
   const page = await context.newPage();
-  await page.goto(`https://${domain}/admin/`);
-  await page.goto(`https://${domain}/admin/#/admin/dashboard`);
+  await page.goto(`${origin}/admin/`);
+  await page.goto(`${origin}/admin/#/admin/dashboard`);
   await page.getByRole('link', { name: 'Environments' }).click();
   await page.getByRole('link', { name: 'Administration' }).click();
   await page.getByRole('link', { name: 'Manage Emulators Emulators' }).click();
@@ -44,6 +56,8 @@ const domain = argv.slice(2);
   await page.getByRole('button', { name: 'Save' }).click();
   await page.getByRole('button', { name: 'Choose action' }).click();
   await page.getByText('Run Environment').click();
+  
+  await page.screenshot("environment.png");
 
   // ---------------------
   await context.close();
