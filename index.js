35bd2cab31aa21b14b8f61b19c3ba3d887daf0c7
Rafael Gieschke <rafael@gieschke.de>
2023-03-06 02:30:32 +0100

    Update index.js

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ const timeout = time_ms => new Promise(r => setTimeout(r, time_ms));
   await page.getByRole('button', { name: 'Choose action' }).click();
   await page.getByText('Run Environment').click();
 
-  await timeout(20_000);
+  await timeout(25_000);
   await page.screenshot({ path: "environment.png" });
 
   // ---------------------
